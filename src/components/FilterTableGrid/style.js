69f3caf5c7882ef21e78aeac8e23171ba9803172
chrisjrd <chris.jrd@gmail.com>
2022-03-15 13:25:48 -0700

    feat(bach-ui): minimized styling on FilterTableGrid
    Refs #16

--- src/components/FilterTableGrid/style.js
+++ src/components/FilterTableGrid/style.js
@@ -1,24 +1,7 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles(() => ({
-  filterWidth: {
-    paddingTop: "width",
-  },
-  gridContainer: {
-    position: "static",
-    alignItems: "flex-start",
-    minHeight: "50%",
-    height: "fit-content",
-    background: "rgb(255, 255, 255)",
-  },
-  leftGridChild: {
-    position: "relative",
-    height: "fit-content",
-    justifyContent: "space-between",
-  },
-  rightGridChild: {
-    position: "relative",
-    height: "fit-content",
-    justifyContent: "space-between",
-  },
+  gridContainer: {},
+  leftGridChild: {},
+  rightGridChild: {},
 }));
