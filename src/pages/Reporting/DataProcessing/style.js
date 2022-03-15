ac1e297f5e448ea7178eaf6a211eb35d89ad0442
chrisjrd <chris.jrd@gmail.com>
2022-03-15 13:10:40 -0700

    feat(bach-ui): removed unused style classes.
    Refs #16

--- src/pages/Reporting/DataProcessing/style.js
+++ src/pages/Reporting/DataProcessing/style.js
@@ -1,33 +1,13 @@
 import { makeStyles } from "@material-ui/core/styles";
-import { NASA_BLUE } from "@bach/constants";
 
-export default makeStyles(() => ({
-  page: {
-    backgroundColor: "#FFFFFF",
-    height: "100vh",
-    marginTop: "35px",
-  },
-  pageContent: {
-    padding: 25,
-  },
-  tabWrapper: {
-    backgroundColor: NASA_BLUE,
-  },
-  tab: {
-    color: "#FFFFFF",
-    fontSize: 15,
-  },
+export default makeStyles((theme) => ({
   subPage: {
-    padding: "0 5px",
     backgroundColor: "#FFFFFF",
   },
-  summaryTable: {
-    marginBottom: "16px",
-    paddingRight: "18px",
-  },
+  summaryTable: {},
   reportTab: {
     backgroundColor: "#FFFFFF",
-    color: NASA_BLUE,
+    color: theme.palette.primary.main,
     height: "100%",
   },
 }));
--- src/pages/Reporting/style.js
+++ src/pages/Reporting/style.js
@@ -1,19 +1,4 @@
-import { NASA_BLUE } from "@bach/constants";
-
 const useStyles = () => ({
-  page: {
-    backgroundColor: "#FFFFFF",
-    height: "100vh",
-  },
-  pageContent: {},
-  tabWrapper: {
-    backgroundColor: NASA_BLUE,
-  },
-  tab: {
-    color: "#FFFFFF",
-    fontSize: 15,
-    marginTop: "25px",
-  },
   subPage: {
     backgroundColor: "#FFFFFF",
   },
