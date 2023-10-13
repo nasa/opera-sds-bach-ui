43febd327819cfcbec34db67c4b43bc6b43263b9
chrisjrd <chris.jrd@gmail.com>
2023-10-13 10:18:01 -0700

    feat(accountability): move to metrics instance
    Refs #634

--- create_config_simlink.sh
+++ create_config_simlink.sh
@@ -1,8 +1,9 @@
 config_path=$1
 bach_ui_path=$2
-source ~/mozart/bin/activate
+source ~/metrics/bin/activate
 [ -f mozartConfig.json ] && rm -f mozartConfig.json
 pip install -e .
-ln -s $config_path $bach_ui_path/config
+# ln -s $config_path $bach_ui_path/config
+grep VENUE $config_path > config
 cp $bach_ui_path/src/config/index.prod.template.js $bach_ui_path/src/config/index.js
 python convert_config.py
